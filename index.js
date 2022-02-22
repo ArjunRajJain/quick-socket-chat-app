4c41480f73ff4263f0886c3ebb74a48a9579a098
Arjun Raj Jain <aj@blockcollider.org>
2022-02-22 13:45:31 -0500

    make it better

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+var rug = require('random-username-generator');
+// rug.setName(['new name']);
+// rug.setAdjectives(['new adjective']);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+var user = rug.generate();
+
+app.get('/initMessages',(req,res)=> {
+  res.json({
+    messages:[
+      {msg:'first message',username:user},
+      {msg:'second message',username:user},
+      {msg:'third message',username:user},
+      {msg:'fourth message',username:user},
+      {msg:'fifth message',username:user},
+    ]
+  })
+})
+
+io.on('connection', (socket) => {
+  socket.emit('username',rug.generate());
+  //receive message from user
+  socket.on('chat message', ({msg,username}) => {
+    io.emit('chat message', {msg,username}); //broadcast message to user
+  });
+});
+
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
